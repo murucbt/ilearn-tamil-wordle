c90c59b555554b0f37e087de94432d776744b937
Rajasekar <rajasekart@cloverbridgetech.com>
2023-08-18 16:12:56 +0530

    text added in footer modal

--- src/components/modals/FooterModal.tsx
+++ src/components/modals/FooterModal.tsx
@@ -9,7 +9,7 @@ import { PhoneIcon } from '@heroicons/react/outline'
       <>
         <div className="flex justify-between gap-4 py-3">
           <div className='footer-links'>
-          Visit <a href="https://ilearntamil.com/" target="_blank" rel="noreferrer"> ilearntamil.com</a> Live online Tamil classes
+          Visit <a href="https://ilearntamil.com/" target="_blank" rel="noreferrer"> ilearntamil.com</a> - Live online Tamil classes
             <div className="socialmedia-links">
               <p className="dark:text-gray-300"><a href="tel:919035394805"><PhoneIcon className="mr-3 h-6 w-6 cursor-pointer dark:stroke-white home-icon"
                 /></a> / <a data-action="open" className="phone-whatsapp" data-phone="919035394805" data-message="" target="_blank" href="https://web.whatsapp.com/send?phone=919035394805&amp;text="><WhatsappLogo className="whatsapp-logo"/> +91  9035394805</a></p>
